f2d6b121979a236c94c5115de815acfe3affee50
Kameron <kfarshchian@gmail.com>
2022-11-09 16:37:23 -0700

    Add Script/css/reset.css. Added html/css/script

---
+++ assets/script.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    {
+        title: "",
+        choices: [""],
+        answer:""
+    },
+    ];
+
+
+function startQuiz(){
+
+}
+
+function timer(){
+
+}
+
+function getQuestion(){
+
+}
+
+function answerPicked(){
+
+}
+
+function quizEnd(){
+
+}
+
+function highScore(){
+
+}
+
+function finalScore(){
+
+}
